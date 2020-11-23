19dbc7f7601e8651766483c176bc0d1c2d53218a
Umarov Kodirbek <52523859+Abumar77@users.noreply.github.com>
2020-11-23 12:56:56 +0500

    Before working properly

---
+++ src/main/java/com/boots/entity/Course.java
@@ -0,0 +1,75 @@
+package com.boots.entity;
+
+
+import javax.persistence.*;
+import java.util.Set;
+
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int cid;
+
+    private String cname;
+
+    private String description;
+
+    private String cprice;
+
+
+    @ManyToMany(mappedBy = "courses")
+    private Set<User> users;
+
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCprice() {
+        return cprice;
+    }
+
+    public void setCprice(String cprice) {
+        this.cprice = cprice;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+   public Course(){
+
+   }
+
+    public Course(String cname, String description, String cprice, Set<User> users) {
+        this.cname = cname;
+        this.description = description;
+        this.cprice = cprice;
+        this.users = users;
+    }
+}
